0d580da403fc297b71f83e898128c086707f122a
Kelly <101299721+florosa21@users.noreply.github.com>
2024-05-14 07:34:09 -0300

    gg

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-console.log(botoes);
+
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
